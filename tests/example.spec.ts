a0ec89ce12bbc9176800443f7a9b91d6f7ab306f
Javaria <javaria.abid@azm.dev>
2025-07-21 14:46:17 +0500

    3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,5 +4,6 @@ test('has title', async ({ page }) => {
   await page.goto('https://merathdcstg.moj.gov.sa/login');
 
   // Expect a title "to contain" a substring.
+  console.log("SDSD")
   // await expect(page).toHaveTitle(/Playwright/);
 });
